0922556c9aefd8c5c1f98df62338a9a8b6193111
akirayou <you.akira.noda@gmail.com>
2021-02-16 08:18:46 +0900

    revert testing code

--- make_dummy_lc_pda.py
+++ make_dummy_lc_pda.py
@@ -12,7 +12,7 @@ rt=1.0
 a=rt/W1*C0
 iso=IsoBET(a,W1,C0,eps=1e-3,alpha=1e-2)
 
-Ws=2.0
+Ws=1.0
 a=rt/Ws
 iso=IsoLang(a,Ws)
 
@@ -20,10 +20,9 @@ iso=IsoLang(a,Ws)
 chrom=SimChrom(100,iso,dead_volume=0,pass_rate=0,inject=inject)
 chrom=np.array([ chrom.step() for i in range(300)])
 chromB=chrom
-chromA=np.roll(chrom,-60)
+chromA=np.roll(chrom,40)*0.5
 
 c=np.linspace(0,1,100)
-
 
 fig=plt.figure()
 ax1 = fig.add_subplot(2, 1, 1)
@@ -32,12 +31,13 @@ plt.plot(c,iso.iso(c))
 
 ax2 = fig.add_subplot(2, 1, 2)
 plt.plot(chromB,label="chrom")
+plt.plot(chromA,label="chrom")
 
 #plt.yscale("log")
 plt.legend()
 plt.pause(0.1)
-plt.show()
-exit(0)
+#plt.show()
+#exit(0)
 #Make dummpy spectrum
 x=np.linspace(0,1,512)
 spA=np.exp(-2*x)*(2+np.sin(x*15))
