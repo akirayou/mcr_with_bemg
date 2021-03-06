d0b2e3c0de24593af0f70f78d70bfbbc630a3137
akirayou <you.akira.noda@gmail.com>
2021-03-06 16:43:52 +0900

    add gauss regressor

--- constraint.py
+++ constraint.py
@@ -5,22 +5,12 @@ from scipy.optimize import curve_fit,minimize
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-def _bemg(x_in,la,lb, s, u,A):
-    a=math.exp(la) #la,lb default 2
-    b=math.exp(lb)
-    x=(x_in-u)/s
-    Ea=(a/2+x)
-    Eb=(b/2-x)
-    Ea[Ea<-25]=-25
-    Eb[Eb<-25]=-25
-    return A*a*b/(a+b)* np.exp(-x**2)* (erfcx(Eb)+erfcx(Ea))
-
-def _fit_bemg(y):
+def _gauss(xi,u,s,A):
+    x=(xi-u)/s
+    return A*np.exp(-x**2)
+def _fit_gauss(y):
     N=len(y)
     x=np.linspace(-1,1,N)
-
-    la=4
-    lb=4
     ui=np.argmax(y)
     u=x[ui]
     A=y[ui]
@@ -29,11 +19,69 @@ def _fit_bemg(y):
     while(0<si and A/2<y[si]):si-=1
     ei=ui
     while(ei<N-2 and A/2 <y[ei] ): ei+=1
-    s=(x[ei]-x[si])/2.86 
+    s=(x[ei]-x[si])/2.355 
 
-    p0=[la,lb,s,u,A]
+    p0=[u,s,A]
+    p,_dummy = curve_fit(_gauss,x,y,p0=p0,bounds=( (-np.inf,1e-2,0 ), (np.inf,np.inf,np.inf)  ))
+    yy=_gauss(x,*p)
+    if False:
+        plt.figure()
+        plt.plot(y,label="in")
+        plt.plot(yy,label="fited")
+        plt.legend()    
+        plt.pause(0.1)
+    return yy
+class ConstraintGauss(pymcr.constraints.Constraint):
+    """
+    Parameters
+    ----------
+    copy : bool
+        Make copy of input data, A; otherwise, overwrite (if mutable)
+    """
+    def __init__(self, peak_indexes , copy=False ):
+        """ A must be non-negative"""
+        super().__init__(copy)
+        self.indexes=peak_indexes
+
+    def transform(self, A):
+        """ Apply nonnegative constraint"""
+        C=A
+        if self.copy:
+            C=np.copy(A)
+        
+        for i in self.indexes:
+            C[:,i]=_fit_gauss(C[:,i])
+        return C
+
+def _bemg(x_in,la,lb, u, s ,A):
+    a=math.exp(la) #la,lb default 2
+    b=math.exp(lb)
+    x=(x_in-u)/s
+    Ea=(a/2+x)
+    Eb=(b/2-x)
+    Ea[Ea<-25]=-25
+    Eb[Eb<-25]=-25
+    return A*a*b/(a+b)* np.exp(-x**2)* (erfcx(Eb)+erfcx(Ea))/1.1273434771994788
+
+def _fit_bemg(y):
+    N=len(y)
+    x=np.linspace(-1,1,N)
+
+    la=6
+    lb=6
+    ui=np.argmax(y)
+    u=x[ui]
+    A=y[ui]
+    
+    si=ui
+    while(0<si and A/2<y[si]):si-=1
+    ei=ui
+    while(ei<N-2 and A/2 <y[ei] ): ei+=1
+    s=(x[ei]-x[si])/2.355  
+
+    p0=[la,lb,u,s,A]
     #print("BEMG intial p0",p0)
-    p,_dummy = curve_fit(_bemg,x,y,p0=p0,bounds=( (-5,-5,1e-2,-np.inf,0 ), (10,10,np.inf,np.inf,np.inf)  ))
+    p,_dummy = curve_fit(_bemg,x,y,p0=p0,bounds=( (-5,-5,-np.inf,1e-2,0 ), (10,10,np.inf,np.inf,np.inf)  ))
     #print("BEMG estimated p",p)
     yy=_bemg(x,*p)
     if False:
@@ -65,14 +113,11 @@ class ConstraintBEMG(pymcr.constraints.Constraint):
             C[:,i]=_fit_bemg(C[:,i])
         return C
 
-
-class FunBEMG():
+class FunGauss():
     def __init__(self):
         pass
     def get_p0(self,x,y):
         N=len(y)
-        la=4
-        lb=4
         ui=np.argmax(y)
         u=x[ui]
         A=y[ui]
@@ -81,12 +126,38 @@ class FunBEMG():
         while(0<si and A/2<y[si]):si-=1
         ei=ui
         while(ei<N-2 and A/2 <y[ei] ): ei+=1
-        s=(x[ei]-x[si])/2.86     
-        return la, lb, s, u, A
+        s=(x[ei]-x[si])/2.355    
+        return  u, s, A
+    def upper_bounds(self):
+        return [np.inf,np.inf,np.inf]
+    def lower_bounds(self):
+        return [-np.inf,1e-2,0]
+    def nof_p(self):
+        return 3
+    def fun(self,x,p):
+        return _gauss(x,*p)
+
+class FunBEMG():
+    def __init__(self):
+        pass
+    def get_p0(self,x,y):
+        N=len(y)
+        la=6
+        lb=6
+        ui=np.argmax(y)
+        u=x[ui]
+        A=y[ui]
+        
+        si=ui
+        while(0<si and A/2<y[si]):si-=1
+        ei=ui
+        while(ei<N-2 and A/2 <y[ei] ): ei+=1
+        s=(x[ei]-x[si])/2.355      
+        return la, lb, u, s, A
     def upper_bounds(self):
         return [10,10,np.inf,np.inf,np.inf]
     def lower_bounds(self):
-        return [-5,-5,1e-2,-np.inf,0]
+        return [-5,-5,-np.inf,1e-2,0]
     def nof_p(self):
         return 5
     def fun(self,x,p):
